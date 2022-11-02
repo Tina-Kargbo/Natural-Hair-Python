b9e91bcfb39dabf7cf58544ac081f0deed57a95a
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-11-02 14:57:59 +0000

    Do validation check to ensure 6 integers only are allowed

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#This code was inspired by the Code Institute's love_sandwiches challenge.
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -32,6 +34,7 @@ def validate_data(values):
     aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) !=6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
