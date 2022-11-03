1e6134bc977a0c7da4e6a5f0523b3a8dc700796d
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-11-03 10:36:16 +0000

    add surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -58,6 +58,15 @@ def update_sales_worksheet(data):
     print("updating sales worksheet...\n") 
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+def update_surplus_worksheet(data):
+
+    print("updating surplus worksheet...\n") 
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -85,6 +94,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data) 
     print(new_surplus_data)
 
 print("Welcome to Natural Hair Products Data Automation")
